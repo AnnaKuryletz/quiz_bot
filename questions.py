1d011afcf91abb1faea88945275ed460ef17bb1d
anyukurilets <anyu.kutilez@mail.ru>
2025-05-29 22:24:53 +0300

    format code

--- questions.py
+++ questions.py
@@ -1,15 +1,15 @@
 import re
 
 
-qaestions_dict = {}
+questions_dict = {}
 
 with open("quiz-questions/1vs1200.txt", "r", encoding="KOI8-R") as my_file:
     file_contents = my_file.read()
 
-qaestions_pairs = re.findall(
+questions_pairs = re.findall(
     r'Вопрос\s*\d*:\s*(.*?)\s*Ответ:\s*(.*?)\s*(?:Автор:|Источник:|$)',
     file_contents,
     re.DOTALL
 )
 
-qa_dict = {q.strip(): a.strip() for q, a in qaestions_pairs}
+qa_dict = {q.strip(): a.strip() for q, a in questions_pairs}
